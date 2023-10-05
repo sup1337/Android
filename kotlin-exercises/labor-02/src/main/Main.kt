02504cfe7a4fc11e788c80f09d18cf8e55766200
sup1337 <64952429+sup1337@users.noreply.github.com>
2023-10-05 11:38:05 +0300

    2

--- kotlin-exercises/labor-02/src/main/Main.kt
+++ kotlin-exercises/labor-02/src/main/Main.kt
@@ -4,16 +4,25 @@ import interfaces.IDictionary
 import ListDictionary
 
 fun main(args: Array<String>) {
-    val dict: IDictionary = ListDictionary
-    println("Number of words: ${dict.size()}")
-    var word: String?
-    while(true){
-        print("What to find? ")
-        word = readLine()
-        if( word.equals("quit")){
-            break
-        }
-        println("Result: ${word?.let { dict.find(it) }}")
-    }
+//    val dict: IDictionary = ListDictionary
+//    println("Number of words: ${dict.size()}")
+//    var word: String?
+//    while(true){
+//        print("What to find? ")
+//        word = readLine()
+//        if( word.equals("quit")){
+//            break
+//        }
+//        println("Result: ${word?.let { dict.find(it) }}")
+//    }
 
+    val name = "Laszlo Hunor"
+    println(name.monogram())
+
+    val words= listOf("apple","pear","plum","strawberry")
+    println(words.joinBySeparator("#"))
+    println(words.longestString())
 }
+fun List<String>.longestString()=this.maxByOrNull { it.length }
+fun String.monogram()=split(" ").map { it.first() }.joinToString("")
+fun List<String>.joinBySeparator(separator: String) = this.joinToString(separator)
